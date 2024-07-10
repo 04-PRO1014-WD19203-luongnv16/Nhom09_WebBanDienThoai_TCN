dfcdb808e5a5e036dad133602d3ffe22ea060cf8
hoangtuyen2004 <ductuyen772@gmail.com>
2024-07-11 00:31:41 +0700

    list sanpham

---
+++ src/Controllers/admins/SanPhamController.php
@@ -0,0 +1,15 @@
+<?php 
+namespace MVC\Controllers\admins;
+
+use MVC\Controller;
+use MVC\Models\SanPham;
+
+class SanPhamController extends Controller {
+    public function index() {
+        $data = [];
+        $data['title'] = "Quản lý sản phẩm";
+        $data['sanphams'] = (new SanPham)->all();
+        return $this->renderAdmin('sanpham/index',$data);
+    }
+}
+?>
