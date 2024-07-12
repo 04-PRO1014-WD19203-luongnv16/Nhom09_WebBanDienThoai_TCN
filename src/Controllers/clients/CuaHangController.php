87ddfef322dae9c8e1049d13958c114ebcd43541
hoangtuyen2004 <ductuyen772@gmail.com>
2024-07-12 13:07:32 +0700

    danh sach san pham

--- src/Controllers/clients/CuaHangController.php
+++ src/Controllers/clients/CuaHangController.php
@@ -2,10 +2,17 @@
 namespace MVC\Controllers\clients;
 
 use MVC\Controller;
+use MVC\Models\SanPham;
 
 class CuaHangController extends Controller {
+    protected $sanphams;
+    public function __construct()
+    {
+        $this->sanphams = (new SanPham);
+    }
     public function index() {
         $data['title'] = "Danh sách sản phẩm";
+        $data['sanphams'] = $this->sanphams->chiTietSanPham();
         return $this->render('client/sanpham/index',$data);
     }
 }
