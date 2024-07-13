f9e7eeded724be52a978514847228fe57ee001dc
hoangtuyen2004 <ductuyen772@gmail.com>
2024-07-13 14:51:58 +0700

    fix loi xung dot

--- src/routes.php
+++ src/routes.php
@@ -20,12 +20,12 @@ $router->addRoute('/login', LoginController::class, 'index');
 $router->addRoute('/logout', LoginController::class, 'logout');
 
 //Route thuộc người dùng
-// $router->addRoute('/list-san_pham', SanPhamController::class, 'index');
-$route->post('/dang_ky', [LoginController::class, 'dangky']);
+$router->addRoute('/list-san_pham', SanPhamController::class, 'index');
+// $route->post('/dang_ky', [LoginController::class, 'dangky']);
 
 //dang ky
 $router->addRoute('/dang_ky', LoginController::class, 'dangky');
-$route->post('/dang_ky',[LoginController::class, 'dangky']);    
+// $route->post('/dang_ky',[LoginController::class, 'dangky']);    
 // SanPham
 
 $router->addRoute('/cua-hang', CuaHangController::class, 'index');
