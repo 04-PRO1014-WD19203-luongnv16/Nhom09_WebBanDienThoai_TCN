5a983e893fdde0e7f3fb23869d8b0366304bb8d0
hoangtuyen2004 <ductuyen772@gmail.com>
2024-07-18 12:10:31 +0700

    gio hang

---
+++ src/Controllers/clients/GioHangController.php
@@ -0,0 +1,40 @@
+<?php 
+namespace MVC\Controllers\clients;
+use MVC\Controller;
+use MVC\Models\BienThe;
+use MVC\Models\GioHang;
+use MVC\Models\SanPham;
+
+class GioHangController extends Controller {
+    protected $giohang;
+    public function __construct() {
+        $this->giohang = new GioHang();
+    }
+    
+    public function index() {
+        if (isset($_SESSION['id'])) {
+            $data['title'] = "Giỏ  hàng";
+        $data['giohangs'] = $this->giohang->selectAll();
+        $data['bienthes'] = (new BienThe)->all();
+        $data['sanphams'] = (new SanPham)->all();
+             $this->render("client/giohang/index",$data);
+        }
+        else {
+            return header('location: /login');
+        }
+    }
+    public function delete() {
+        if (isset($_SESSION['id'])) {
+            if (isset($_POST['submit'])) {
+                $giohangs = $this->giohang->selectAll();
+                foreach ($giohangs as $giohang) {
+                    if ($giohang['id'] == $_POST['id']) {
+                        (new GioHang)->delete($_POST['id']);
+                        header('location: /gio-hang');
+                    }
+                }
+            }
+        }
+    }
+}
+?>
--- src/Models/BienThe.php
+++ src/Models/BienThe.php
@@ -15,4 +15,29 @@ class BienThe extends Model
         $this->setQuery($sql);
         return $this->GetAll();
     }
+
+    // SELECT by id
+    public function one($id) {
+        $sql = "SELECT bien_thes.id,bien_thes.id_san_phams,bien_thes.id_mau_sacs,mau_sacs.ten_mau_sac,bien_thes.id_dung_luongs,dung_luongs.ten_dung_luong,bien_thes.so_luong,bien_thes.gia_goc,bien_thes.gia_ban FROM $this->table
+        INNER JOIN mau_sacs ON bien_thes.id_mau_sacs = mau_sacs.id 
+        INNER JOIN dung_luongs ON bien_thes.id_dung_luongs = dung_luongs.id WHERE bien_thes.id = $id;";
+    }
+
+    public function selectByID($id) {
+        $sql = "SELECT * FROM $this->table WHERE id = $id";
+        $this->setQuery($sql);
+        return $this->GetOne();
+    }
+
+    // SELECT by id_san_phams
+    public function selectBy($id_san_phams) {
+        $sql = "SELECT * FROM $this->table WHERE id_san_phams = $id_san_phams";
+        $this->setQuery($sql);
+        return $this->GetAll();
+    }
+    public function getId($id_san_phams = "", $id_mau_sacs = "", $id_dung_luongs = "") {
+        $sql = "SELECT * FROM $this->table WHERE id_san_phams = '$id_san_phams' AND id_mau_sacs = '$id_mau_sacs' AND id_dung_luongs = '$id_dung_luongs'";
+        $this->setQuery($sql);
+        return $this->GetOne();
+    }
 }
